48dbd77ef187074a3bf68e5bf0c2f4cd610d1da7
Mateus Valgueiro <mvt@cesar.org.br>
2020-10-08 17:45:03 -0300

    faet - add global styles file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        data: `@import "${__dirname}/src/styles/globals";`,
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
