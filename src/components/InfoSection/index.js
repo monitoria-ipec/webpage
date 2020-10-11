6c0b79628ad5fbab49d0f31706e12feb576c842e
Mateus Valgueiro <mvt@cesar.org.br>
2020-10-10 21:03:11 -0300

    chore - update info section style

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,38 +1,33 @@
 import React from "react";
 // import { Link } from "gatsby";
 import "./styles.scss";
-import { MdClass } from "react-icons/md";
-const InfoSection = ({ usefulLinks }) => {
+
+const InfoSection = () => {
   return (
     <div className="info-container">
       <div className="info-about-section">
-        {/* mudar isso aqui depois para ser um dos mds */}
-        <h2 className="info-about-section--header">Sobre o site</h2>
-        <p className="info-about-section--text">
-          Fizemos esse site com o objetivo de facilitar a comunicação entre
-          monitores e estudantes da cadeira de introdução a programação para
-          engenharia da computação. Aqui, você pode encontrar as redes socias de
-          todos os monitores, além de arquivos, como slides e provas antigas, e
-          links úteis que os ajudarão nos estudos da cadeira.
-        </p>
-        <div>
-          <iframe
-            width="500"
-            height="300"
-            class="video"
-            src="https://www.youtube.com/embed/nKIu9yen5nc"
-            frameborder="0"
-            title="Video"
-            allowfullscreen
-          ></iframe>
+        <div className="info-about-panel">
+          <h1 className="info-about-panel--header">Sobre o site</h1>
+          <div className="info-about-panel--text">
+            Fizemos esse site com o objetivo de facilitar a comunicação entre
+            monitores e estudantes da cadeira de introdução a programação para
+            engenharia da computação. Aqui, você pode encontrar as redes socias
+            de todos os monitores, além de arquivos, como slides e provas
+            antigas, e links úteis que os ajudarão nos estudos da cadeira.
+          </div>
         </div>
       </div>
-
-      <div className="useful-links-section">
-        <h2 className="info-about-section--header">Links Úteis</h2>
-
-        <div className="icon">
-          <MdClass />
+      <div className="info-about-video-section">
+        <h2 className="info-about-video-section--header">Inspire-se</h2>
+        <div className="info-about-video-section">
+          <iframe
+            src="https://www.youtube.com/embed/nKIu9yen5nc"
+            frameBorder="0"
+            width="500"
+            height="300"
+            title="Video"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
     </div>
