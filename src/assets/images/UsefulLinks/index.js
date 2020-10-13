739824dac368fbfaf2faac4620cb395803783ff2
rosig <rglj2@cin.ufpe.br>
2020-10-12 21:14:19 -0300

    style - finalizing useful links sec (responsive)

--- src/assets/images/UsefulLinks/index.js
+++ src/assets/images/UsefulLinks/index.js
@@ -1,39 +1,6 @@
-const progressivec = require('./c.png');
-const stackoverflow = require('./stack.jpg');
-const disciplina = require('./disciplina.png');
-const vscode = require('./vscode.png');
-const thehuxley = require('./theH.png');
-const codeblocks = require('./codeblocks.png');
-
-export default [
-  {
-    name: 'C Progressivo',
-    url: 'http://www.cprogressivo.net/',
-    image: progressivec,
-  },
-  {
-    name: 'Stack Overflow',
-    url: 'http://stackoverflow.com/',
-    image: stackoverflow
-  },
-  {
-    name: 'Site da Disciplina',
-    url: 'http://stackoverflow.com/',
-    image: disciplina
-  },
-  {
-    name: 'Visual Studio Code',
-    url: 'https://code.visualstudio.com/',
-    image: vscode
-  },
-  {
-    name: 'The Huxley',
-    url: 'https://www.thehuxley.com/',
-    image: thehuxley
-  },
-  {
-    name: 'CodeBlocks',
-    url: 'http://www.codeblocks.org/downloads/26',
-    image: codeblocks
-  }
-]
+export const progressivec = require('./c.png');
+export const stackoverflow = require('./stack.jpg');
+export const disciplina = require('./disciplina.png');
+export const vscode = require('./vscode.png');
+export const thehuxley = require('./theH.png');
+export const codeblocks = require('./codeblocks.png');
---
+++ src/components/UsefulLinksSection/data.js
@@ -0,0 +1,34 @@
+import {progressivec, stackoverflow, disciplina, vscode, thehuxley, codeblocks} from '../../assets/images/UsefulLinks';
+
+export default [
+  {
+    name: 'C Progressivo',
+    url: 'http://www.cprogressivo.net/',
+    image: progressivec,
+  },
+  {
+    name: 'Stack Overflow',
+    url: 'http://stackoverflow.com/',
+    image: stackoverflow
+  },
+  {
+    name: 'Site da Disciplina',
+    url: 'https://sites.google.com/a/cin.ufpe.br/if669ec/',
+    image: disciplina
+  },
+  {
+    name: 'Visual Studio Code',
+    url: 'https://code.visualstudio.com/',
+    image: vscode
+  },
+  {
+    name: 'The Huxley',
+    url: 'https://www.thehuxley.com/',
+    image: thehuxley
+  },
+  {
+    name: 'CodeBlocks',
+    url: 'http://www.codeblocks.org/downloads/26',
+    image: codeblocks
+  }
+]
