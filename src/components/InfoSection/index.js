ae45cafdcadabfa97c1bc65900fee9a5887c65e9
rosig <rglj2@cin.ufpe.br>
2020-10-21 17:18:36 -0300

    refactor - updating infoSection html and css

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -5,28 +5,32 @@ const InfoSection = () => {
   return (
     <div className="info-container">
       <div className="info-about-section">
-        <div className="info-about-panel">
-          <h1 className="info-about-panel--header">Sobre o site</h1>
-          <div className="info-about-panel--text">
+        <div className="info-about-title">
+          <h1>Sobre o site</h1>
+        </div>
+        <div className="info-about-text">
+          <p>
             Fizemos esse site com o objetivo de facilitar a comunicação entre
             monitores e estudantes da cadeira de introdução a programação para
             engenharia da computação. Aqui, você pode encontrar as redes socias
             de todos os monitores, além de arquivos, como slides e provas
             antigas, e links úteis que os ajudarão nos estudos da cadeira.
-          </div>
+          </p>
         </div>
       </div>
-      <div className="info-about-video-section">
-        <h2 className="info-about-video-section--header">Inspire-se</h2>
-        <div className="info-about-video-section">
+      <div className="info-video-section">
+        <div className='info-video-title'>
+          <h1>Inspire-se</h1>
+        </div>
+        <div className="info-video-youtube">
           <iframe
-            src="https://www.youtube.com/embed/nKIu9yen5nc"
-            frameBorder="0"
-            width="500"
-            height="300"
-            title="Video"
-            allowFullScreen
-          ></iframe>
+              src="https://www.youtube.com/embed/nKIu9yen5nc"
+              frameBorder="0"
+              width="500"
+              height="300"
+              title="Video"
+              allowFullScreen
+            />
         </div>
       </div>
     </div>
