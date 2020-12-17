97997f4349e234815dc466b839599aedeecfdabc
Valgueiro <Valgueiro@users.noreply.github.com>
2020-12-17 23:00:47 +0000

    deployed via Gatsby Publish Action 🎩 for 2aab0b8b0a1ca288d4d5a5baa8955e105fca9193

---
+++ component---src-pages-arquivos-js-f958b6213d4b46ed44e0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{plXo:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),o=n.n(a),r=n("Zttt");e.default=function(){return o.a.createElement(r.a,{title:"Arquivos"},o.a.createElement("h2",null,"Arquivos"))}}}]);
+//# sourceMappingURL=component---src-pages-arquivos-js-f958b6213d4b46ed44e0.js.map
